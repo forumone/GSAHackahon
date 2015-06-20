c8848bf35a327f33c21083a2f0e4740a0cd2a8e7
William Hurley <whurley@forumone.com>
2015-06-20 15:26:15 -0400

    Adding select for drugs

--- assets/js/interface/states/home/index.js
+++ assets/js/interface/states/home/index.js
@@ -1,22 +1,11 @@
 angular.module('gsa18f').controller('HomeController', function($scope, fdaLabel) {
-  $scope.currentPage = 0;
-  $scope.limit = 20;
-  $scope.total = 0;
+  $scope.drugs = [{
+    drug : '',
+  }];
   
-  getLabels();
-  
-  $scope.pagingAction = function(page) {
-    $scope.currentPage = page;
-    getLabels();
-  }
-  
-  function getLabels() {
-    var skip = $scope.currentPage * $scope.limit;
-
-    fdaLabel.search('', skip, $scope.limit).then(function(results) {
-      $scope.results = results;
-      $scope.drugLabels = results.results;
-      $scope.total = results.meta.results.total;
-    })
+  $scope.addDrug = function() {
+    $scope.drugs.push({
+      drug : ''
+    });
   }
 });
