b974d932c53e4d2fff75ea7c8adc2f544657ab02
William Hurley <whurley@forumone.com>
2015-06-24 13:38:23 -0400

    Minor refactoring of bar chart to not be stacked

--- assets/js/interface/components/charts/bar/index.js
+++ assets/js/interface/components/charts/bar/index.js
@@ -1,17 +1,28 @@
 angular.module('gsa18f').directive(
     'chartsBar',
     function() {
-      var BarChart = nv.models.multiBarHorizontalChart();
-      
-      BarChart.x(function(d) { return d.label });
-      BarChart.y(function(d) { return d.value });
-      
-      BarChart.yAxis
-          .tickFormat(d3.format(',.1f'));
-      
-      BarChart.multibar.stacked(true); // default to stacked
-      BarChart.showControls(false); // don't show controls
+      function buildChart(element, data) {
+        var BarChart = nv.models.multiBarHorizontalChart();
+        
+        BarChart
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .showControls(false) // don't show controls
+          .margin({
+            left : 125
+          })
+          .yAxis.tickFormat(d3.format(',.1f'));
+        
+        d3.select(element[0]).select("svg").remove();
+        var svg = d3.select(element[0])
+          .append("svg")
+          .datum(data)
+          .transition().duration(500).call(BarChart);
 
+        // TODO: Ensure that windowResize event is detached when the directive is destroyed
+        nv.utils.windowResize(BarChart.update);
+      }
+      
       return {
         restrict : 'E',
         scope : {
@@ -29,64 +40,20 @@ angular.module('gsa18f').directive(
             scope.width = 500;
           }
 
-          var colorscale = d3.scale.category10();
-
-          // Legend titles
-          var LegendOptions = [ 'Smartphone', 'Tablet' ];
-
-          // Data
-          var data = [{
-            key: "Drug1",
-            color: "#00A6B4",
-            values: [
-              {
-                label: "Effect A",
-                value: 3
-              },
-              {
-                label: "Effect B",
-                value: 4
-              },
-              {
-                label: "Effect C",
-                value: 5
-              },
-              {
-                label: "Effect D",
-                value: 6
-              }
-            ]},
-            {
-            key: "Drug2",
-            color: "#8AC35D",
-            values: [
-              {
-              label: "Effect A",
-              value: 1
-              },
-              {
-              label: "Effect B",
-              value: 2
-              },
-              {
-              label: "Effect C",
-              value: 3
-              },
-              {
-              label: "Effect D",
-              value: 4
-              }
-            ]}
-          ];
-
-          scope.$watch('height+width', function() {
-            d3.select(element[0]).select("svg").remove();
-            d3.select(element[0])
-              .append("svg")
-                .datum(data)
-                .transition().duration(500).call(BarChart);
-            // TODO: Ensure that windowResize event is detached when the directive is destroyed
-            nv.utils.windowResize(BarChart.update);
+          scope.$watch('height+width+data', function() {
+            scope.styles = {
+              height : scope.height
+            };
+            
+            if (angular.isDefined(scope.data)) {
+              buildChart(element.find('div'), scope.data);
+            }
+          });
+          
+          scope.$watchCollection('data', function() {
+            if (angular.isDefined(scope.data)) {
+              buildChart(element.find('div'), scope.data);
+            }
           });
         }
       };
