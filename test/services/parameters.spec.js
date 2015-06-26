76e847f9a7f9045ca7d3fca31e87c51572898509
Alex Ford <aford@forumone.com>
2015-06-26 17:17:44 -0400

    Add tests for parameters.getQueryParameters()

--- test/services/parameters.spec.js
+++ test/services/parameters.spec.js
@@ -2,6 +2,57 @@ var parameters = require('../../api/services/parameters'),
     assert = require('assert');
     
 describe('services/parameters', function () {
+    describe('getQueryParams()', function () {
+        it('omits "All"', function () {
+            var query = {
+                x: 'All',
+            };
+            
+            var result = parameters.getQueryParams({ query: query }, ['x'], {});
+            assert.deepEqual(result, {}, 'expected no values');
+            
+            query = {
+                x: 'All',
+                y: 'All',
+            };
+            
+            result = parameters.getQueryParams({query: query}, ['x', 'y'], {});
+            assert.deepEqual(result, {}, 'expected no values'); 
+        });
+        
+        it('returns only requested values', function () {
+            var query = {
+                x: 'x',
+                y: 'y',
+                z: 'z',
+            };
+            
+            var result = parameters.getQueryParams({ query: query }, ['x', 'y'], {});
+            assert.deepEqual(result, {x: 'x', y: 'y'}, 'expected only "x" and "y" keys');
+        });
+        
+        it('returns defaults', function () {
+            var query = {},
+                defaults = {x: 'x', y: 'y'};
+            
+            var result = parameters.getQueryParams({ query: query }, ['x', 'y'], defaults);
+            assert.deepEqual(result, {x: 'x', y: 'y'}, 'defaulted parameters');
+        });
+        
+        it('overwrites defaults', function () {
+            var query = {
+                    x: 'overwritten',
+                },
+                defaults = {
+                    x: 'defaulted',
+                    y: 'defaulted',
+                };
+                
+            var result = parameters.getQueryParams({ query: query }, ['x', 'y'], defaults);
+            assert.deepEqual(result, { x: 'overwritten', y: 'defaulted' }, 'defaulted and overwritten parameters');
+        });
+    });
+    
     describe('serialize()', function () {
         it('handles empty objects', function () {
             var result = parameters.serialize({});
